b017b64bd39af8fb4bb7b680a4a5169ba5f52ac2
thomassdk <thomassdk@pm.me>
2022-06-16 18:26:57 +0200

    Init repo

---
+++ src/lib/Header.svelte
@@ -0,0 +1,21 @@
+
+<nav>
+    <a href="photos">Photos</a>
+    <a href="details">Details</a>
+    <a href="location">Location</a>
+    <a href="accomodation">Accomodation</a>
+    <a href="schedule">Schedule</a>
+</nav>
+
+<style>
+ nav {
+     display: flex;
+     justify-content: center;
+     align-items: center;
+     gap: 20px;
+     position: sticky;
+     top: 0;
+     padding-block: 1rem;
+     background-color: white;
+ }
+</style>
