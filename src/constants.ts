a47a69c1b97c36578da0b8c410391350d73cb5b2
thomassdk <thomassdk@pm.me>
2022-06-18 09:52:19 +0200

    Added media queries to constants

--- src/constants.ts
+++ src/constants.ts
@@ -15,3 +15,15 @@ export const ELEVATIONS = {
     16px 32px 32px hsl(var(--shadow-color) / 0.2)
   `
 }
+
+export const BREAKPOINTS = {
+    phone: 600,
+    tablet: 950,
+    laptop: 1300,
+};
+
+export const QUERIES = {
+    phoneAndSmaller: `(max-width: ${BREAKPOINTS.phone / 16}rem)`,
+    tabletAndSmaller: `(max-width: ${BREAKPOINTS.tablet / 16}rem)`,
+    laptopAndSmaller: `(max-width: ${BREAKPOINTS.laptop / 16}rem)`,
+};
