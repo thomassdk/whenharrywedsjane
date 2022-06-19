15e10c873c278d586a4415f811fa06060fedbc3f
thomassdk <thomassdk@pm.me>
2022-06-19 18:19:34 +0200

    change start time to 2:15

--- src/lib/Schedule.svelte
+++ src/lib/Schedule.svelte
@@ -12,7 +12,7 @@
 
     const Events = [
         {
-            date: new Date("2022-09-28T14:30:00"),
+            date: new Date("2022-09-28T14:15:00"),
             text: "Guests begin arriving",
         },
         {
